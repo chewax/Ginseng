372f91a5548a537f6743bd648ad770090ad97305
Daniel Waksman <dwaksman@gmail.com>
2019-12-29 09:25:47 -0300

    Fixed dependency error ont tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,10 +1,8 @@
 #define BOOST_TEST_MODULE GinsengTEST
-#include <boost/test/included/unit_test.hpp>
-#include <iostream>
-#include <math.h>
+#include "../boost/test/included/unit_test.hpp"
 
-BOOST_AUTO_TEST_CASE(Test_Vector3)
+BOOST_AUTO_TEST_CASE(Test1)
 {
-  
+  BOOST_CHECK_EQUAL(1,1);  
 }
 
