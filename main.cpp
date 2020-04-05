382ada5d528bd761ba414cdc756263fd4ff4f8db
Daniel Waksman <dwaksman@gmail.com>
2020-04-05 11:19:48 -0300

    Changed CLI to REPL

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 
 int main()
 {
-  // Ginseng cli("$test>");
-  	Ginseng cli("$test>", []()
+  // Ginseng repl("$test>");
+  	Ginseng repl("$test>", []()
 	{
-		std::cout << "\nWELCOME TO MY AWESOME CLI!" << "\n";
+		std::cout << "\nWELCOME TO MY AWESOME REPL!" << "\n";
 		std::cout << "Type \"help\" to start" << "\n";
 		std::cout << "" << "\n";
 		std::cout << "HAVE FUN!" << "\n";
@@ -16,10 +16,10 @@ int main()
 	{
 		std::cout << "\nBye My friend!" << "\n";
 	});
-  // Ginseng cli;
+  // Ginseng repl;
   
   Help hello_h("Says hello back at you", "[name]");
-  cli.add_command("hello", [](std::vector<std::string> args) -> int
+  repl.add_command("hello", [](std::vector<std::string> args) -> int
   { 
     if (args.size() < 2) return Exit::INVALID_ARGUMENTS;
     std::cout << "HELLO " << args[1] << std::endl; 
@@ -27,13 +27,13 @@ int main()
   }, hello_h);
 
   Help goodbye_h("Says goodbye!", "[]");
-  cli.add_command("goodbye", [](std::vector<std::string> args) -> int
+  repl.add_command("goodbye", [](std::vector<std::string> args) -> int
   { 
     std::cout << "GOODBYE!" << std::endl; 
     return Exit::SUCCESS;
   }, goodbye_h);
 
-  cli.start();
+  repl.start();
   
   return 0;
 }
